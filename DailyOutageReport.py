bf338a4d1fec8828c078b0b47d80681dc613cc17
Akib Hossain <76858240+akib16301028@users.noreply.github.com>
2025-01-15 05:34:48 +0600

    Update DailyOutageReport.py

--- DailyOutageReport.py
+++ DailyOutageReport.py
@@ -225,3 +225,4 @@ if rms_site_file and alarm_history_file and grid_data_file and total_elapse_file
 # Final Message
 if rms_site_file and alarm_history_file and grid_data_file and total_elapse_file:
     st.sidebar.success("All files processed and merged successfully!")
+ 
